bd11daf3ab300e24e3c930a85d15ec2d3f9486ec
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-06 08:58:26 +0530

    roboshop project

--- tools-setup/module/spot-vm/main.tf
+++ tools-setup/module/spot-vm/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "public-ip" {
   location              = var.location
   resource_group_name   = var.resource_group_name
   allocation_method     = "Dynamic"
-  sku                   = "Standard"
+  sku                   = "Basic"
 }
 
 resource "azurerm_network_interface" "privateip" {
