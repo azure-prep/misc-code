0c838a44fa8c0b537d5eec903f1a000c5288e014
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-08 08:06:26 +0530

    roboshop project

--- vault/main.tf
+++ vault/main.tf
@@ -1,15 +1,15 @@
 provider vault{
-  address = "https://vault-int.azdevopsb1.online:8200"
+  address = "http://vault-int.azdevopsb1.online:8200"
   token   = var.token
 }
 
-resource "vault_mount" "kvv2" {
-  for_each = var.secret
-  path        = each.key
-  type        = "kv"
-  options     = { version = "2" }
-  description = "KV Version 2 secret engine mount"
-}
+# resource "vault_mount" "kvv2" {
+#   for_each = var.secret
+#   path        = each.key
+#   type        = "kv"
+#   options     = { version = "2" }
+#   description = "KV Version 2 secret engine mount"
+# }
 
 # resource "vault_kv_secret_v2" "example" {
 #   mount                      = vault_mount.kvv2.path
