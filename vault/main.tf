53905f66059f2bf0121dc1497e9c62491a730c84
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-08 08:17:13 +0530

    roboshop project

--- vault/main.tf
+++ vault/main.tf
@@ -3,13 +3,13 @@ provider vault{
   token   = var.token
 }
 
-# resource "vault_mount" "kvv2" {
-#   for_each = var.secret
-#   path        = each.key
-#   type        = "kv"
-#   options     = { version = "2" }
-#   description = "KV Version 2 secret engine mount"
-# }
+resource "vault_mount" "kvv2" {
+  for_each = var.secret
+  path        = each.key
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
 
 # resource "vault_kv_secret_v2" "example" {
 #   mount                      = vault_mount.kvv2.path
