973091fa2233b5ae568a443bc620009c5e9da19f
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-08 07:55:45 +0530

    roboshop project

--- vault/main.tf
+++ vault/main.tf
@@ -3,24 +3,42 @@ provider vault{
   token   = var.token
 }
 
-resource "vault_mount" "secret" {
+resource "vault_mount" "kvv2" {
   for_each = var.secret
-  namespace = each.key
-  path      = "secrets"
-  type      = "kv"
-  options = {
-    version = "2"
-  }
+  path        = each.key
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
 }
 
-resource "vault_generic_secret" "example" {
-  for_each = var.values
-  path = "${each.value["secret"]}/${each.key}"
+# resource "vault_kv_secret_v2" "example" {
+#   mount                      = vault_mount.kvv2.path
+#   name                       = "secret"
+#   cas                        = 1
+#   delete_all_versions        = true
+#   data_json                  = jsonencode(
+#     {
+#       zip       = "zap",
+#       foo       = "bar"
+#     }
+#   )
+#   custom_metadata {
+#     max_versions = 5
+#     data = {
+#       foo = "vault@example.com",
+#       bar = "12345"
+#     }
+#   }
+# }
 
-  data_json = jsonencode(
-    {
-      "foo"   = "bar",
-      "pizza" = "cheese"
-    }
-  )
-}
+# resource "vault_generic_secret" "example" {
+#   for_each = var.values
+#   path = "${each.value["secret"]}/${each.key}"
+#
+#   data_json = jsonencode(
+#     {
+#       "foo"   = "bar",
+#       "pizza" = "cheese"
+#     }
+#   )
+# }
