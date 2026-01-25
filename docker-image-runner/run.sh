6cd3b83b9bd4a945142c1b23696e0d364cc70416
chandralek.singasani <chandralek.singasani@servicenow.com>
2026-01-26 00:03:43 +0530

    roboshop project

--- docker-image-runner/run.sh
+++ docker-image-runner/run.sh
@@ -1,3 +1,3 @@
 ./config.sh --url https://github.com/azure-prep --token ${TOKEN} --unattended --name ${NAME}
-sudo ./svc.sh install
-sudo ./svc.sh start
+#sudo ./svc.sh install
+#sudo ./svc.sh start
